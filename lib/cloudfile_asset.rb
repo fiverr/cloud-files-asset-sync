01a1f495b588cd9c7650f298ada4a8dda7c25b8f
Elad Meidar <elad@eizesus.com>
2012-03-06 14:13:56 +0200

    [FIX] removing RAILS_ROOT

--- lib/cloudfile_asset.rb
+++ lib/cloudfile_asset.rb
@@ -6,5 +6,5 @@ require 'cloudfile_asset/container'
 
 module CloudfileAsset
   # defined?(Rails.public_path) ? Rails.public_path : "public"
-  LOCAL_PUBLIC_PATH = "#{RAILS_ROOT}/public/"
+  LOCAL_PUBLIC_PATH = "#{Rails.root}/public/"
 end
